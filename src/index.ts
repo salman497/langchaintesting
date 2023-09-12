07d8d28aa26f5dd4105f790bf363462f15ee35a0
Salman Aziz <salman.aziz@nintex.com>
2023-09-12 22:49:48 +1000

    git update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { run } from "./examples/few_shot";
+// import { run } from "./examples/custom_llm_agent_chat";
+
 
 console.log("----------------Executing------------------");
 run().catch(error => {
