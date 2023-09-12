09f223e69b0b945147c3b95b4f59f55727db5344
Salman Aziz <salman.aziz@nintex.com>
2023-09-12 22:41:20 +1000

    chore: testing

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello, TypeScript!");
+import { run } from "./few_shot";
+
+console.log("----------------Executing------------------");
+run().catch(error => {
+    console.error("Error running the async function:", error);
+  });
